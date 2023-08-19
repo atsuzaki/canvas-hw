85d8bad9c6dae4d3a6fd298fa67a0c3530cde9c9
Bart Massey <bart.massey@gmail.com>
2023-08-18 23:53:15 -0700

    fixed unpacker to handle fragmented group subs with dup files

--- unpack-hw.py
+++ unpack-hw.py
@@ -1,7 +1,8 @@
 # Unpack Canvas HW for grading.
 # Bart Massey 2023
 
-import argparse, re, json, subprocess, sys, zipfile
+import argparse, json, re, subprocess, sys, zipfile
+from collections import defaultdict
 from pathlib import Path
 
 parser = argparse.ArgumentParser(
@@ -20,6 +21,14 @@ def mkfdir(root):
     fdest.mkdir(mode=0o700)
     return fdest
 
+def read_link(arch, member):
+    linky = str(arch.read(member))
+    linkinfo = link_re.search(linky)
+    if not linkinfo:
+        print(f"warning: link not parsed: {member.filename}", file=sys.stderr)
+        return None
+    return linkinfo[0]
+
 sdest = mkfdir("staged")
 gdest = mkfdir("graded")
 
@@ -30,39 +39,72 @@ with open("students.json", "r") as s:
         student_id = str(record["id"])
         student_names[student_id] = record["short_name"]
 
-filename_re = re.compile(r"([a-z]+)_(LATE_)?([0-9]+)_.*$")
+filename_re = re.compile(r"([a-z]+)_(LATE_)?([0-9]+)_([0-9]+|link).*$")
+link_re = re.compile(r'href="(https://git[^"/]*/[^"/]+/[^"/]+)(/[^"]*)?"')
 project_archive = zipfile.ZipFile(args.filename)
-projects = []
+
+groups = defaultdict(lambda: set())
 for zipinfo in project_archive.infolist():
     fields = filename_re.search(zipinfo.filename)
     if fields is None:
         print(f"warning: filename not parsed: {zipinfo.filename}", file=sys.stderr)
         continue
-    slug, student_id = fields[1], fields[3]
-    name = student_names[student_id]
+    slug, student_id, tag = fields[1], fields[3], fields[4]
+    if tag == "link":
+        tag = read_link(project_archive, zipinfo)
+        if tag is None:
+            continue
 
+    groups[tag].add((slug, student_id))
+
+groups = set(frozenset(g) for g in groups.values())
+students = set(s for g in groups for s in g)
+for s in students:
+    assert len(set(g for g in groups if s in g)) == 1
+
+paths = dict()
+for g in groups:
+    slugs = sorted(list(m[0] for m in g))
+    slug = '-'.join(slugs)
     path = sdest / slug
-    had_dir = path.is_dir()
-    if not had_dir:
-        path.mkdir(mode=0o700)
-        
-    if Path(zipinfo.filename).suffix in ("ZIP", "zip"):
+    for s in slugs:
+        paths[s] = path
+
+    path.mkdir(mode=0o700, exist_ok=True)
+
+    ids = ''.join(sorted(list(f"{m[0]}={m[1]}\n" for m in g)))
+    with open(path / ".ids", "w") as f:
+        f.write(ids)
+
+    names = ', '.join(sorted(list(student_names[m[1]] for m in g)))
+
+    grading = path / "GRADING.txt"
+    with open(grading, "w") as gr:
+        print(args.hw, file=gr)
+        print(names, file=gr)
+        print("-", file=gr)
+        print(file=gr)
+
+seen = set()
+for zipinfo in project_archive.infolist():
+    fields = filename_re.search(zipinfo.filename)
+    if fields is None:
+        continue
+    slug, student_id, tag = fields[1], fields[3], fields[4]
+    link = tag == "link"
+    if link:
+        tag = read_link(project_archive, zipinfo)
+        if tag is None:
+            continue
+    if tag in seen:
+        continue
+    seen.add(tag)
+
+    path = paths[slug]
+
+    if Path(zipinfo.filename).suffix in (".ZIP", ".zip"):
         zip = project_archive.open(zipinfo)
         with zipfile.ZipFile(zip) as zip:
             zip.extractall(path=path)
     else:
         project_archive.extract(zipinfo, path=path)
-
-    if had_dir:
-        continue
-
-    grading = path / "GRADING.txt"
-    with open(grading, "w") as gr:
-        print(args.hw, file=gr)
-        print(name, file=gr)
-        print("-", file=gr)
-        print(file=gr)
-
-    info = path / ".student_id"
-    with open(info, "w") as i:
-        print(f"{student_id}", file=i)
